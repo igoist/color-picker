1f19e873a7e0b75329a984091e46c64ecf48313a
igoist <493512132@qq.com>
2019-06-21 16:30:42 +0800

    Complete getClipData and drawPoint

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 实际上有些包是不需要被编译打包的，如 electron、react、react-dom，到时候在 html 中直接引用 lib.min.js
 
 编译过程移除的问题后续考虑
+
+
+## 遇到的问题
+
+getClipData 卡了好久，奇怪为什么超过一定数值，data[xxx] 获取到的 r g b a 都是 0
+
+实际上是一开始 tmpCanvas 没有设置 width, height
+
+导致了 getScreenSources 的 callback 中 tmpContext.drawImage 绘制内容完全溢出了
--- src/index.tsx
+++ src/index.tsx
@@ -2,33 +2,183 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
-import '../style.css';
+import '../public/css/style.css';
 
 import getScreenSources from './desktop-capturer';
 
 import { getCurrentScreen } from './utils/';
 
 let currentScreen = getCurrentScreen();
-// let scaleFactor = currentScreen.scaleFactor;
+let scaleFactor = currentScreen.scaleFactor;
 let screenWidth = currentScreen.bounds.width;
 let screenHeight = currentScreen.bounds.height;
+
+let tmpImg = new Image();
+let tmpCanvas: HTMLCanvasElement = document.getElementById('tmp') as HTMLCanvasElement;
+let tmpCanvasData: any = null;
 
 getScreenSources({}, (imgSrc: any) => {
 
   let bgImg = document.getElementById('bgImg');
-  console.log('bgImg: ', bgImg);
 
   bgImg.style.backgroundImage = `url(${ imgSrc })`;
   bgImg.style.backgroundSize = `${ screenWidth }px ${ screenHeight }px`;
+
+  tmpImg.src = imgSrc;
+  tmpImg.addEventListener('load', function() {
+    tmpCanvas.width = tmpImg.width;
+    tmpCanvas.height = tmpImg.height;
+    let tmpContext: any = tmpCanvas.getContext('2d');
+
+    tmpContext.drawImage(tmpImg, 0, 0, tmpImg.width, tmpImg.height);
+    tmpCanvasData = tmpContext.getImageData(0, 0, tmpImg.width, tmpImg.height);
+  }, false);
 });
 
+const body = document.body;
+const radius = 80;
+const borderSize = 2;
+const range = 11;
+const clipRange = Math.ceil((2 * radius) / range);
+let tmpClipData: (string[] | null) = null;
 
 
-console.log(getScreenSources);
+interface GetClipDataConfig {
+  position: { x: number, y: number };
+  imgData: any;
+}
+
+const getClipData: any = (config: GetClipDataConfig) => {
+  const { position, imgData } = config;
+  let clipData = [];
+  let [x, y] = [~~(position.x * scaleFactor - radius / range), ~~(position.y * scaleFactor - radius / range)];
+  let data = imgData.data;
+
+  let index, r, g, b, a;
+
+  if (!data) return;
+  (window as any).data = data;
+
+  for (let i = 0; i < clipRange; i ++) {
+    for (let j = 0; j < clipRange; j ++) {
+      index = ~~((y + i) * imgData.width + x + j);
+      r = data[index * 4 + 0];
+      g = data[index * 4 + 1];
+      b = data[index * 4 + 2];
+      a = data[index * 4 + 3] / 255;
+      clipData.push(`rgba(${r},${g},${b},${a})`);
+    }
+  }
+
+  return clipData;
+}
+
+interface DrawPointConfig {
+  canvasElement: any;
+  clipData: any; //string[];
+}
+
+const drawPoint = (config: DrawPointConfig) => {
+  const { canvasElement, clipData } = config;
+
+  let ctx = canvasElement.getContext('2d');
+  let resize = radius - range * clipRange / 2;
+  let length = clipData.length;
+  let current = ~~(length / 2);
+  let x, y, cp: any = {};
+  ctx.save();
+  for (let i = 0 ; i < length; i ++) {
+    y = ~~(i / clipRange);
+    x = i - y * clipRange;
+    if (i === current) {
+      cp = {
+        x: x * range + resize,
+        y: y * range + resize
+      };
+    } else {
+      ctx.lineWidth = 0.1;
+      // ctx.strokeStyle = 'rgba(255,255,255,0.4)'
+      ctx.strokeStyle = 'rgba(5,5,5,0.4)';
+      ctx.fillStyle = clipData[i];
+      ctx.fillRect(x * range + resize, y * range + resize, range, range);
+      ctx.strokeRect(x * range + resize, y * range + resize, range, range);
+      ctx.restore();
+    }
+  }
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = '#fc04db';
+  ctx.fillStyle = clipData[current];
+  ctx.fillRect(cp.x, cp.y, range, range);
+  ctx.strokeRect(cp.x, cp.y, range, range);
+  ctx.restore();
+}
+
+
+const ClipView = () => {
+  const cE = React.useRef(null);
+
+  const [state, setState] = React.useState({ top: `-${ radius }px`, left: `-${ radius }px` });
+
+  const onMouseMove = (e: any) => {
+    let position = {
+      x: e.clientX - body.offsetLeft,
+      y: e.clientY - body.offsetTop
+    };
+
+    if (tmpCanvasData) {
+      tmpClipData = getClipData({
+        position: position,
+        imgData: tmpCanvasData,
+      });
+
+      setState({
+        top: `${ position.y - radius - borderSize }px`,
+        left: `${ position.x - radius - borderSize }px`
+      });
+    }
+  }
+
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('Unbind body\'s mousemove');
+      body.removeEventListener('mousemove', onMouseMove);
+    }
+
+    body.addEventListener('mousemove', onMouseMove);
+  }, []);
+
+  React.useEffect(() => {
+    if (tmpClipData) {
+      drawPoint({
+        canvasElement: cE.current,
+        clipData: tmpClipData
+      });
+    }
+  });
+
+  return (
+    <div
+      className='clip-view'
+      style={{
+        top: state.top,
+        left: state.left,
+        width: `${ 2 * radius + 2 * borderSize }px`,
+        height: `${ 2 * radius + 2 * borderSize }px`,
+        // backgroundColor: '#CCCFFF' // this line for code test
+      }}
+    >
+      <canvas ref={ cE } width='160' height='160'></canvas>
+      <span>#FFFFFF</span>
+    </div>
+  )
+}
+
+
 ReactDOM.render(
   <AppContainer>
     <div>
-      SS
+      <ClipView />
+      <p>SSd</p>
     </div>
   </AppContainer>,
   document.getElementById('app')
